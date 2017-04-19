7794125455200c65e7e9bb99ba417244af42468f
Rosario Felix <elijan_11@hotmail.com>
2017-04-19 14:01:51 -0500

    subiendo cancha faltan pelotas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,22 @@
 window.addEventListener("load",function() {
 
+var contenedor= document.getElementById('contenedor');
+
+var cancha=document.createElement("div");
+cancha.classList.add("cancha");
+
+var canchaHijo=document.createElement("div");
+canchaHijo.classList.add("cancha-hijo");
+
+var arcoUno=document.createElement("div");
+arcoUno.classList.add("arco-uno");
+
+var arcoDos=document.createElement("div");
+arcoDos.classList.add("arco-dos");
 
 
-  
+contenedor.appendChild(cancha);
+cancha.appendChild(canchaHijo);
+canchaHijo.appendChild(arcoUno);
+canchaHijo.appendChild(arcoDos);
 });
